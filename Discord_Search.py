e3b6e48e03e61e1d55f91f65f70657cb4900849a
pie5434 <sallypoon22@gmail.com>
2019-02-18 19:59:49 -0800

    Takes top 5 searches

--- Discord_Search.py
+++ Discord_Search.py
@@ -10,21 +10,35 @@ TOKEN = 'NTQ2MTIxMTgyMjk0ODM1MjEw.D0jmmg.EW2sBmhkW452XZe7jYhIT76SirQ'
 
 client = discord.Client()
 
+url = 'http://wiki.mabinogiworld.com/api.php'
+def searchWiki(query):
+    links = 3
+    response = requests.get(url, params = {"action": "opensearch",
+    		"format": "json",
+    		"formatversion": 2,
+    		"search": query,
+    		"namespace": 0,
+    		"limit": 5,
+    		"suggest": True,})
+    results = response.json()
+    string = "\n".join("{} {}".format(text, url) for text, url in zip(results[1], results[links]))
+        # string = "\n".join("{} {}".format(b))
+        # string = "\n".join("{} {}".format(c))
+        # string = "\n".join("{} {}".format(d))
+        # string = "\n".join("{} {}".format(e))
+    return string
+            # http://wiki.mabinogiworld.com/api.php?action=opensearch&
+            # format=json&search=query&namespace=0&limit=10&suggest=True)
+
+
+
 @client.event
 async def on_message(message):
-    def __repr__(self):
-        return str(self.__dict__)
     if message.content.startswith('$search'):
-        search =  message.content.split(maxsplit = 1)
-        search = search[1:];
-        response = requests.get('//wiki.mabinogiworld.com/api.php')
-        print(response.status_code);
-        #await client.send_message(message.channel, search)
-        # message = await client.wait_for_message(author=message.author, check=check)
-        # name = message.content[len('$name'):].strip()
-        # await client.send_message(message.channel, '{} is stinky'.format(name))
-
-
+        words =  message.content.split(maxsplit = 1)
+        words = words[1:]
+        await client.send_message(message.channel, searchWiki(words[0]))
+        #await client.send_message(message.channel, results)
 
 @client.event
 async def on_ready():
